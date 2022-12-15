e7f9b6ffc52ebd3b714feae6f0c3dc9793225a6c
Blendi <bd52288@ubt-uni.net>
2022-12-15 23:02:58 +0100

    Database, Unit
    Krijimi Databazes dhe perfundimi i Unit klases

---
+++ InventoryManagement/Controllers/UnitController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventoryManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using InventoryManagement.Data;
+
+namespace InventoryManagement.Controllers
+{
+
+    public class UnitController : Controller
+    {
+        private readonly InventoryContext _context;
+
+        public UnitController(InventoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Units.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Units == null)
+            {
+                return NotFound();
+            }
+
+            var unit = await _context.Units
+                .FirstOrDefaultAsync(u => u.Id == id);
+            if (unit == null)
+            {
+                return NotFound();
+            }
+
+            return View(unit);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description")] Unit unit)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(unit);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(unit);
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Units == null)
+            {
+                return NotFound();
+            }
+
+            var unit= await _context.Units.FindAsync(id);
+            if (unit == null)
+            {
+                return NotFound();
+            }
+            return View(unit);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Unit unit)
+        {
+            if (id != unit.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(unit);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UnitExists(unit.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(unit);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Units == null)
+            {
+                return NotFound();
+            }
+
+            var unit = await _context.Units
+                .FirstOrDefaultAsync(u => u.Id == id);
+            if (unit == null)
+            {
+                return NotFound();
+            }
+
+            return View(unit);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Units == null)
+            {
+                return Problem("Entity set 'InventoryContext.Units'  is null.");
+            }
+            var unit = await _context.Units.FindAsync(id);
+            if (unit != null)
+            {
+                _context.Units.Remove(unit);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UnitExists(int id)
+        {
+            return _context.Units.Any(e => e.Id == id);
+        }
+    }
+}
