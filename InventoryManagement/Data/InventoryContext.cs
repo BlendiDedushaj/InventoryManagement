80fe223683bfdad3eed3bb6f46ae7ddca4ae7b53
Blendi <bd52288@ubt-uni.net>
2022-12-15 23:32:34 +0100

    Squashed commit of the following:
    commit e7f9b6ffc52ebd3b714feae6f0c3dc9793225a6c
    Author: Blendi <bd52288@ubt-uni.net>
    Date:   Thu Dec 15 23:02:58 2022 +0100
    
        Database, Unit
    
        Krijimi Databazes dhe perfundimi i Unit klases
    
    commit e7c2b21c6fb1134aece71390e010a85a21399a52
    Author: Blendi <bd52288@ubt-uni.net>
    Date:   Tue Dec 13 16:09:50 2022 +0100
    
        First commit
    
    commit 70929a00003d8d8a6717ac401ba65fa65e210b9a
    Author: Blendi <bd52288@ubt-uni.net>
    Date:   Tue Dec 13 16:08:26 2022 +0100
    
        First commit

---
+++ InventoryManagement/Data/InventoryContext.cs
@@ -0,0 +1,12 @@
+﻿using InventoryManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Data
+{
+    public class InventoryContext : DbContext
+    {
+        public InventoryContext(DbContextOptions options): base(options) { }
+
+        public virtual DbSet<Unit> Units { get; set; }
+    }
+}
--- InventoryManagement/Program.cs
+++ InventoryManagement/Program.cs
@@ -1,8 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
+using InventoryManagement.Data;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<InventoryContext>(options =>
+    options.UseSqlServer(connectionString));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
