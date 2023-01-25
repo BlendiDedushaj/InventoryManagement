afd50e21e9212206dd0288fd0e704f132960bae5
Andin <ab51129@ubt-uni.net>
2023-01-26 00:37:05 +0100

    Modal Dialog Bug Fixes
    PopUp Message Box update

--- InventoryManagement/wwwroot/js/site.js
+++ InventoryManagement/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function openErrorModal(strMessage) {
+    var myDiv = document.getElementById("MyModalErrorAlertBody");
+    myDiv.innerHTML = strMessage;
+    $('#myModalError').modal('show');
+}
+
+function openSuccessModal(strMessage) {
+    var myDiv = document.getElementById("MyModalSuccessAlertBody");
+    myDiv.innerHTML = strMessage;
+    $('#myModalSuccess').modal('show');
+}
