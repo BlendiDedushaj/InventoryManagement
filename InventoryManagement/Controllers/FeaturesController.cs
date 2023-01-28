e6a6fc1c3d4873bb869fa2c99fca34f42df3fed1
ArtinaBektashi <93448372+ArtinaBektashi@users.noreply.github.com>
2023-01-28 17:44:28 +0100

    features page added

---
+++ InventoryManagement/Controllers/FeaturesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.Controllers
+{
+    public class FeaturesController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public FeaturesController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
